b3f04f3a0714711d3c48c556c9afca12930bb8d5
weverthonmachado <weverthonmachado@gmail.com>
2015-05-01 23:54:58 -0300

    best.R funcionando

--- ProgrammingAssignment3/best.R
+++ ProgrammingAssignment3/best.R
@@ -1,7 +1,22 @@
 best <-function(state, outcome) {
-      outcome<-read.csv("outcome-of-care-measures.csv", colClasses="character")
-      states<-levels(as.factor(outcome[,7]))
+      data<-read.csv("outcome-of-care-measures.csv", 
+                     colClasses="character")[,c(2, 7, 11, 17, 23)]
+      states<-levels(as.factor(data[,2]))
       
+      if (!(state %in% states)) stop("invalid state")
+      if (!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) {
+            stop("invalid outcome")
+      }
       
-
+      out<- if (outcome=="heart attack") {
+            3
+            } else if (outcome=="heart failure") {
+            4
+            } else {
+            5
+      }
+      statedata<-data[data[,2]==state,]
+      statedata[statedata=="Not Available"]<-NA
+      i<-order(as.numeric(statedata[,out]), rev(statedata[,1]), na.last=NA)[1]
+      statedata[i, 1]
 }
